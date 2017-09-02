fe994b1faba8a0d88686881520b01ea22a4bb714
tanglinbing <tanglinbing@4399inc.com>
2017-09-02 11:14:12 +0800

    add java class file and source to test networke classloader

---
+++ tmp/Test.java
@@ -0,0 +1,19 @@
+package tmp;
+
+/**
+ * @author Linbing Tang
+ * @since 2017-09-02
+ */
+public class Test {
+    static {
+        System.out.println("tmp.Test: static block");
+    }
+
+    public static void show(){
+        System.out.println("tmp.Test: public static void show()");
+    }
+
+    public void method(){
+        System.out.println("tmp.Test: public void method()");
+    }
+}
